450e4f8a9a13d3abba2c523343991a40019d96f7
atikapuspitasari <kaaatesting@gmail.com>
2023-12-01 09:43:57 +0700

    Add test case

---
+++ src/test/java/id/co/pkp/App1Test.java
@@ -0,0 +1,98 @@
+package id.co.pkp;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class App1Test {
+    @Test
+    @DisplayName("Test Web PKP")
+    public void test1() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch();
+        Page page = browser.newPage();
+        page.navigate("https://www.google.co.id");
+        System.out.println("Page Title ini adalah " + page.title());
+    }
+
+    @Test
+    @DisplayName("Check URL or Check HTTPS")
+    public void testCheckHTTPS() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("http://10.8.11.1:8761/");
+        String currentUrl = page.url();
+        String expectedUrl = "https://10.8.11.1:8761/";
+        if (currentUrl.equals(expectedUrl)) {
+            System.out.println("URL is correct: " + currentUrl);
+        } else {
+            System.out.println("URL is incorrect. Expected: " + expectedUrl + ", but got: " + currentUrl);
+        }
+        // System.out.println(currentUrl);
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    @DisplayName("Check Place Holder")
+    public void checkPlaceHolder() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("https://www.programsbuzz.com/user/login");
+
+        Locator searchBar = page.locator("#edit-keys");
+        String placeText = searchBar.getAttribute("placeholder");
+
+        if (placeText.contains("Enter the terms you wish to search for")) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL! No such texts");
+        }
+    }
+
+    @Test
+    @DisplayName("Assert Check Box")
+    public void assertCheckBox() {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+        page.navigate("http://autopract.com/selenium/dropdown1/");
+        Locator locator = page.locator("select.custom-select option >> nth=-2");
+        String attributeV = locator.getAttribute("value");
+
+        if (attributeV.equals("item3")) {
+            System.out.println("Attribute value is correct!");
+        } else {
+            System.out.println("Attribute value is incorrect.");
+        }
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    @DisplayName("Tooltip Check Test")
+    public void tooltipCheckTest() {
+        Playwright playwright = Playwright.create();
+        BrowserContext browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false))
+                .newContext();
+
+        Playwright playwright = Playwright.create();
+        BrowserContext browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false))
+                .newContext();
+
+        Page page = browser.newPage();
+        page.navigate("https://jqueryui.com/tooltip/");
+        FrameLocator frameOne = page.frameLocator(".demo-frame");
+        Locator ageBox = frameOne.locator("#age");
+        Locator toolTipText = frameOne.locator(".ui-tooltip-content");
+        ageBox.hover();
+        String textContent = toolTipText.textContent();
+        System.out.println(textContent);
+
+        browser.close();
+        playwright.close();
+    }
+}
+
